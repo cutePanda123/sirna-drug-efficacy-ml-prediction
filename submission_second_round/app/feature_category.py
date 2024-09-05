7f92346ef412fe5f895914bfddc1913ff020eba6
Ruizhong Miao <49664448+RuizhongMiao@users.noreply.github.com>
2024-09-05 06:32:56 -0700

    Pre-specify variable categories (#20)
    For features which pd.get_dummies was applied to, added there pre-specified categories in the feature_category.py file.

--- submission_second_round/app/feature_category.py
+++ submission_second_round/app/feature_category.py
@@ -9,6 +9,13 @@ publication_id_category = ['WO0b0a8fec3d', 'WO0beeb1f407', 'WO181a7d3a16', 'WO18
        'WOda137ca368', 'WOdbb8b7dbd0', 'WOdc4f13c160', 'WOdc6f16a65e',
        'WOee0afbdeee', 'WOf5583f6d6e', 'WOf5fa77d982', 'WOff4c21cc8c']
 
+gene_target_symbol_name_category = ['ABLIM3', 'ACE2', 'ADRA2A', 'ANGPTL3', 'ANGPTL4', 'ANGPTL7', 'APP',
+       'ATXN2', 'Angptl7', 'Atxn2', 'C3', 'C9', 'CFB', 'CIDEB', 'CTNNB1',
+       'Cfb', 'F10', 'F12', 'F5', 'FLCN', 'FLNA', 'Flcn', 'Flna', 'GSK3A',
+       'Gsk3a', 'HAO1', 'HMGCR', 'Hao1', 'KHK', 'LDHA', 'Ldha', 'MAPT',
+       'MUC5B', 'Mtarc1', 'Muc5b', 'PRNP', 'PRODH2', 'SCD5', 'SCN9A', 'SLC5A2',
+       'SOD1', 'Sod1', 'TMPRSS2', 'TMPRSS6', 'TTR']
+
 gene_target_ncbi_id_category = ['NM_000064.3', 'NM_000130.4', 'NM_000311.5', 'NM_000371.4',
        'NM_000454.4', 'NM_000484.4', 'NM_000504.4', 'NM_000505.4',
        'NM_000681.4', 'NM_000859.3', 'NM_001031667.1', 'NM_001037582.3',
@@ -22,6 +29,12 @@ gene_target_ncbi_id_category = ['NM_000064.3', 'NM_000130.4', 'NM_000311.5', 'NM
        'NM_019884.3', 'NM_021146.4', 'NM_021232.2', 'NM_021804.3',
        'NM_028801.2', 'NM_144997.7', 'NM_146018.2', 'NM_153609.4',
        'XM_005272484.3', 'XM_005593037.2', 'XM_006527911.5', 'XM_017004061.1']
+
+gene_target_species_category = ['Homo sapiens', 'Macaca fascicularis', 'Mus musculus']
+
+Transfection_method_category = ['Free uptake', 'Lipofectamine']
+
+Duration_after_transfection_h_category = [24.0, 48.0]
 
 cell_line_donor_category = ['A549 Cells', 'BE(2)-C Cells', 'COS-7 Cells', 'DU145 Cells',
        'HeLa Cells', 'Hep3B Cells', 'HepG2 Cells', 'Hepa1-6 Cells',
